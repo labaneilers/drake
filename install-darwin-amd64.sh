2956903904eca6ea600cfc6be2bbff11569d43e5
Laban Eilers <laban@eilers.com>
2018-07-26 13:43:22 -0400

    cleanup mac installation

--- install-darwin-amd64.sh
+++ install-darwin-amd64.sh
@@ -1,3 +1,5 @@
+set -e
+
 version=$(curl -s https://github.com/labaneilers/drake/releases | grep releases.*windows-amd64 | sed -n -E 's/^.*download\/(.*)\/windows.*$/\1/p')
 fileurl="https://github.com/labaneilers/drake/releases/download/${version}/darwin-amd64.drk"
 
@@ -5,9 +7,21 @@ installdir="/usr/local/bin"
 mkdir -p $installdir
 
 filepath="${installdir}/drk"
+versionpath="${filepath}.version"
 
-echo $filepath
-echo $fileurl
+echo "Latest version: ${version}"
 
-wget $fileurl -O $filepath
+if [ -f $filepath ] && [ -f $versionpath ]; then
+    diskVersion=$(cat $versionpath)
+    if [ $version == $diskVersion ]; then
+        echo "Already up-to-date"
+        exit
+    fi
+fi
+
+echo "Downloading from ${fileurl}..."
+
+wget --quiet $fileurl -O $filepath
 chmod 777 $filepath
+
+echo $version > $versionpath
