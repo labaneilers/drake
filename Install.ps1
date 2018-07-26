9a8b63732bc1d6801d3d1aa21b9af0db8a8ec743
Laban Eilers <laban@eilers.com>
2018-07-26 13:22:36 -0400

    Windows installation improvements

--- Install.ps1
+++ Install.ps1
@@ -1,30 +1,39 @@
+$ErrorActionPreference = "Stop"
 
 $installDir = Join-Path $env:ALLUSERSPROFILE "drake"
 if (![System.IO.Directory]::Exists($installDir)) {[void][System.IO.Directory]::CreateDirectory($installDir)}
 $filePath = Join-Path $installDir "drk.exe"
-
-# $r = Invoke-RestMethod -uri "https://api.github.com/repos/labaneilers/drake/releases/latest"
-# $downloadUri = $r.assets | ? { $_.name -match 'windows-amd64' } | % { $_.browser_download_url }
+$versionFilePath = "$($filePath).version"
 
 function Get-LatestVersion() {
-    $r = (iwr "https://github.com/labaneilers/drake/releases").Content
-    $line = $r.Split("`n") | ? { $_ -match "download.*windows-amd64" }
+    $r = (Invoke-WebRequest "https://github.com/labaneilers/drake/releases").Content
+    $line = $r.Split("`n") | Where-Object { $_ -match "download.*windows-amd64" }
     $found = $line -match 'download\/(.*)\/windows-amd64'
     if (! $found) {
         throw "Error"
     }
-    
     $matches[1]
 }
 
 $version = Get-LatestVersion
+"Latest version: $version"
+
+if ((Test-Path $filePath) -and (Test-Path $versionFilePath)) {
+    $diskVersion = Get-Content $versionFilePath
+    if ($version -eq $diskVersion) {
+        "Already up-to-date"
+        exit
+    }
+}
+
 $downloadUri = "https://github.com/labaneilers/drake/releases/download/$($version)/windows-amd64.drk.exe"
-
-
+"Downloading from $downloadUri..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri $downloadUri -outfile $filePath
 
-Write-Output 'Ensuring drk is on the path'
+$version > $versionFilePath
+
+"Ensuring $installDir is on the path..."
 $path = [Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine);
 if ($path.ToLower().Contains($installDir.ToLower()) -eq $false) {
   $path = $path + $installDir
---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Installation
+
+### Windows
+
+```
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/labaneilers/drake/master/Install.ps1'))
+```
